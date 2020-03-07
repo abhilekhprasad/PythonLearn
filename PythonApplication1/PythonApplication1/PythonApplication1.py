c4a8306ab50ea1f62db7a5451e9c5f73a9612e4f
christian <christian@bale.com>
2020-03-07 22:18:42 +0530

    progressing with list

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,5 +1,11 @@
-
 cars = ['bmw','audi','toyota','subaru']
 print(cars)
-cars.sort()
+cars.reverse() #reversing the actual order of the list, and not reverse sorting
+print(cars) #printing list
+print(sorted(cars)) #sorting only for printing
 print(cars)
+cars.sort() #sorting the actual list
+print(cars)
+cars.sort(reverse= True) #reverse sorting the list
+print(cars)
+print(len(cars)) #printing the length of the list
